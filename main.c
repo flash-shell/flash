caa1d4c8adbfec150308a3a587e34feb0eafb038
Angus Logan <angus.logan.2020@uni.strath.ac.uk>
2022-01-21 14:48:18 +0000

    brought comments in line with convention

--- main.c
+++ main.c
@@ -40,34 +40,38 @@ int main(void) {
 
         printf("%s@%s$ ", USERNAME, HOSTNAME);
 
-        /**
-            sizeof input is used rather than a hard-coded variable, for good practice.
-        */
+        /*
+         *  sizeof input is used rather than a hard-coded variable, for good practice.
+         */
 
         if(fgets(input, sizeof input, stdin)) {
             tmp[i] = strtok(input, delims);
 
             while(tmp[i] != NULL) {
-                /**
-                    This if statement here checks to see if the last character in the buffer "tmp[i]"
-                    is the newline character, and if it is it then removes the character, allowing
-                    for comparisons.
-                */
+                /*
+                 *  This if statement here checks to see if the last character in the buffer "tmp[i]"
+                 *  is the newline character, and if it is it then removes the character, allowing
+                 *  for comparisons.
+                 */
 
                 if ((p = strchr(tmp[i], '\n')) != NULL)
                     *p = '\0';
 
-                /**
-                    This last if statement, compares the value of tmp[i] to the word "exit", hence checks
-                    whether the user has typed "exit", if so, cont will be set to false and the shell
-                    will cease to run.
-                */
+                /*
+                 *  This last if statement, compares the value of tmp[i] to the word "exit", hence checks
+                 *  whether the user has typed "exit", if so, cont will be set to false and the shell
+                 *  will cease to run.
+                 */
 
                 if (strcmp(tmp[i], "exit") == 0)
                     cont = false;
 
                 i++;
-                tmp[i] = strtok(NULL, delims); // Here, "NULL" is passed as the first arg instead of the input, as strtok() has an internal state (static pointer) which remembers the last input.
+                /*
+                 *  Here, "NULL" is passed as the first arg instead of the input, as strtok() has an
+                 *  internal state (static pointer) which remembers the last input.
+                 */
+                tmp[i] = strtok(NULL, delims); 
             }
         }
     }
