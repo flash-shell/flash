04313aa5b83506fc7d5246fe12ca2ed509c609db
Angus Logan <angus.logan@baronsview.com>
2022-01-28 11:19:57 +0000

    implemented Andrew's change to file structure, committed this with changed branch

---
+++ io_handler.h
@@ -0,0 +1,5 @@
+void display_prompt();
+void break_to_command(char **token, int *tokenCount);
+void break_to_arg(char **arg, int *argCount, char *input);
+int first_quote_occurence(char **args, int *argCount, int quote);
+void handle_commands(char **token, int no_commands);
--- tokenise.h
+++
@@ -1,3 +0,0 @@
-void break_to_command(char **token, int *tokenCount);
-void break_to_arg(char **arg, int *argCount, char *input);
-int first_quote_occurence(char **args, int *argCount, int quote);
